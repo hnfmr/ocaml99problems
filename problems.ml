35dddcd85ebe5e950d47dc29cff917471ff43a7e
Wei Qi <qiwei.uk@gmail.com>
2013-11-03 10:09:54 +0800

    change file name

--- problems.ml
+++ problems.ml
@@ -169,10 +169,3 @@ assert(encode3 ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"] =
        [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d";
         Many (4, "e")]);;
 
-
-
-
-
-
-
-
