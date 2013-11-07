0434431a64a0d9382d577ec1cae72f3a17af2baa
Wei Qi <qiwei.uk@gmail.com>
2013-11-07 22:51:09 +0800

    Finished Arithmetic part..

--- arithmetic.ml
+++ arithmetic.ml
@@ -1,5 +1,6 @@
 let is_prime n =
   match n with
+    | 0 -> false
     | 1 -> false
     | 2 -> true
     | _ -> 
@@ -30,6 +31,9 @@ let phi m =
   let d = aux 1 [] in
     List.length d;;
 
+phi 10;;
+phi 13;;
+
 let factors m =
   let rec aux c a d =
     if c = m then a else
@@ -41,4 +45,103 @@ let factors m =
 
 factors 315;;
 
+let factors2 n =
+  let rec aux d n =
+    if n = 1 then [] else
+    if n mod d = 0 then
+      match aux d (n / d) with
+        | (h, n) :: t when h = d -> (h, n + 1) :: t
+        | l -> (d, 1) :: l
+    else
+      aux (d+1) n
+  in
+    aux 2 n;;
 
+factors2 315;;
+
+let rec power x n =
+  match n with
+    | 0 -> 1
+    | 1 -> x
+    | _ -> if n mod 2 = 0 then (power x (n/2)) * (power x (n/2))
+        else x * (power x (n-1));;
+
+power 10 3;;
+power 10 4;;
+
+let phi_improved m =
+  let factors = factors2 m in
+    List.fold_left (fun a (h, n) -> a * (h - 1) * (power h (n-1))) 1 factors;;
+
+factors2 10;;
+phi_improved 10;;
+phi_improved 13;;
+
+#load "unix.cma";;
+open Unix;;
+
+let timeit f a =
+  let t0 = Unix.gettimeofday() in
+    ignore(f a);
+    let t1 = Unix.gettimeofday() in
+      t1 -. t0;;
+
+timeit phi 10090;;
+timeit phi_improved 10090;;
+
+#use "basic.ml";;
+
+range 2 10;;
+let all_primes a b =
+  let l = range a b in
+  let rec aux l a =
+    match l with
+      | [] -> a
+      | h :: t -> if is_prime h then aux t (h :: a) else aux t a
+  in
+    aux l [];;
+
+List.length (all_primes 2 7920);;
+
+(* extremely inefficient algorithm for goldbach, gasp*)
+let goldbach n =
+  let primes = all_primes 2 n in
+  let rec aux l a =
+    match l with
+      | [] -> a
+      | h :: t -> let appended = List.map (fun x -> (h, x)) primes in
+            aux t (appended @ a)
+  in
+  let l = aux primes [] in
+  let rec find l acc =
+    match l with
+      | [] -> acc
+      | (a, b) :: t -> if a + b = n then find [] ((a, b) :: acc) else find t acc
+  in
+    List.hd (find l []);;
+
+
+factors 28;;
+goldbach 10;;
+goldbach 28;;
+
+let goldbach2 n =
+  let rec aux d =
+    if is_prime d && is_prime (n-d) then (d, n-d)
+    else aux (d+1) in
+    aux 2;;
+
+goldbach2 28;;
+
+let goldbach_list a b =
+  let ints = range a b in
+  let even_ints = List.filter (fun x -> x mod 2 = 0) ints in
+    List.map (fun x -> (x, goldbach2 x)) even_ints;;
+
+goldbach_list 9 20;;
+
+let goldbach_limit a b k =
+  let l = goldbach_list a b in
+    List.filter (fun (u, (v, w)) -> v > k && w > k) l;;
+
+goldbach_limit 4 2000 50;;
